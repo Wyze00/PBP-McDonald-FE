f0ec9ec52dfbaeb1b28f888559613dcef51cc498
Wyze00 <nathantanusubroto15@gmail.com>
2026-04-03 12:34:24 +0700

    Init

---
+++ src/redux/template.slice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Template } from "../types/template.type";
+
+const initialState: Template = '';
+
+type TemplateSliceAction = {
+    type: string,
+    payload: Template | undefined
+}
+
+export const templateSlice = createSlice({
+    name: 'template',
+    initialState,
+    reducers: {
+        setState: (_, action: TemplateSliceAction) => {
+            return action.payload;
+        } 
+    }
+})
