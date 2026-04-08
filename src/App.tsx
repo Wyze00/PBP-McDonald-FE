c693ad73ec6b51a3d8af201cc45d37ea6a895044
Wyze00 <nathantanusubroto15@gmail.com>
2026-04-08 17:54:59 +0700

    create AdminMenu page

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { store } from "./redux/store";
 import Header from "./components/Header";
 import PublicRoute from "./middlewares/public.middleware";
+import AdminMiddleware from "./middlewares/admin.middleware";
 
 const HomePage = lazy(() => import('./pages/HomePage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
 const NotFound = lazy(() => import('./pages/NotFound'));
+const AdminHomePage = lazy(() => import('./pages/admin/AdminHomepage'));
+const AdminMenu = lazy(() => import('./pages/admin/AdminMenu'));
 
 export default function App(): React.JSX.Element {
     return (
@@ -18,6 +21,10 @@ export default function App(): React.JSX.Element {
                 <Route path="/" element={<HomePage />} />
               <Route element={<PublicRoute />}>
                 <Route path="/login" element={<LoginPage />} />
+              </Route>
+              <Route element={<AdminMiddleware />}>
+                <Route path="/admin" element={<AdminHomePage />}/>
+                <Route path="/admin/menu" element={<AdminMenu />}/>
               </Route>
               <Route path="*" element= {<NotFound />} />
             </Routes>
